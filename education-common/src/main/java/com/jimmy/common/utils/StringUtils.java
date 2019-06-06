85987637b5dc966c06ad5abd712b900be7333736
yanyifei <yanyifei@zcckj.com>
2019-06-06 10:41:48 +0800

    初始化

---
+++ education-common/src/main/java/com/jimmy/common/utils/StringUtils.java
@@ -0,0 +1,10 @@
+package com.jimmy.common.utils;
+
+/**
+ * @ClassName: StringUtils
+ * @Description:
+ * @author: aiden
+ * @date: 2019/6/6/006.
+ */
+public class StringUtils extends org.apache.commons.lang3.StringUtils {
+}
---
+++ education-core/src/main/java/com/jimmy/core/anno/LogInfo.java
@@ -0,0 +1,16 @@
+package com.jimmy.core.anno;
+
+
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface LogInfo {
+    int type();
+
+    int operation();
+
+    String param() default "";
+
+}
