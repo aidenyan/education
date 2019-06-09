52f519a413caab565d3fdc4e1a6c828d2ee9a56f
yanyifei <yanyifei@zcckj.com>
2019-06-09 21:25:39 +0800

    初始化

--- education-dao/src/main/java/com/jimmy/dao/entity/CourseInfo.java
+++ education-dao/src/main/java/com/jimmy/dao/entity/CourseInfo.java
@@ -1,9 +1,12 @@
 package com.jimmy.dao.entity;
 
+import lombok.Data;
+
 import java.util.Date;
 
-public class CourseInfo {
-    private Long id;
+@Data
+public class CourseInfo extends BaseEntity {
+
 
     private String name;
 
@@ -13,103 +16,10 @@ public class CourseInfo {
 
     private Long roomId;
 
-    private Long createId;
+    private Long teacherId;
 
-    private Long modifyId;
-
-    private Date createTime;
-
-    private Date modifyTime;
-
-    private Long siteId;
+    private Boolean isUsed;
 
     private String description;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name == null ? null : name.trim();
-    }
-
-    public Date getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(Date startTime) {
-        this.startTime = startTime;
-    }
-
-    public Integer getTimeLong() {
-        return timeLong;
-    }
-
-    public void setTimeLong(Integer timeLong) {
-        this.timeLong = timeLong;
-    }
-
-    public Long getRoomId() {
-        return roomId;
-    }
-
-    public void setRoomId(Long roomId) {
-        this.roomId = roomId;
-    }
-
-    public Long getCreateId() {
-        return createId;
-    }
-
-    public void setCreateId(Long createId) {
-        this.createId = createId;
-    }
-
-    public Long getModifyId() {
-        return modifyId;
-    }
-
-    public void setModifyId(Long modifyId) {
-        this.modifyId = modifyId;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getModifyTime() {
-        return modifyTime;
-    }
-
-    public void setModifyTime(Date modifyTime) {
-        this.modifyTime = modifyTime;
-    }
-
-    public Long getSiteId() {
-        return siteId;
-    }
-
-    public void setSiteId(Long siteId) {
-        this.siteId = siteId;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description == null ? null : description.trim();
-    }
 }
--- education-dao/src/main/java/com/jimmy/dao/mapper/CourseInfoMapper.java
+++ education-dao/src/main/java/com/jimmy/dao/mapper/CourseInfoMapper.java
@@ -1,19 +1,61 @@
 package com.jimmy.dao.mapper;
 
 import com.jimmy.dao.entity.CourseInfo;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface CourseInfoMapper {
-    int deleteByPrimaryKey(Long id);
 
+    /**
+     * 保存课程信息
+     *
+     * @param record 课程信息
+     * @return 保存信息
+     */
     int insert(CourseInfo record);
 
-    int insertSelective(CourseInfo record);
+    /**
+     * 查找课程信息
+     *
+     * @param id ID
+     * @return 课程信息
+     */
+    CourseInfo findById(@Param("id") Long id, @Param("siteIdList") List<Long> siteIdList);
 
-    CourseInfo selectByPrimaryKey(Long id);
+    /**
+     * 查找课程信息
+     *
+     * @param name ID
+     * @return 课程信息
+     */
+    List<CourseInfo> list(@Param("name") String name, @Param("siteIdList") List<Long> siteIdList);
 
-    int updateByPrimaryKeySelective(CourseInfo record);
+    /**
+     * 更新课程信息
+     *
+     * @param record 课程信息
+     * @return 更新数量
+     */
+    int update(CourseInfo record);
 
-    int updateByPrimaryKeyWithBLOBs(CourseInfo record);
+    /**
+     * 更新需要更新的数量
+     *
+     * @param record 课程信息
+     * @return 更新数量
+     */
+    int updateProperty(CourseInfo record);
 
-    int updateByPrimaryKey(CourseInfo record);
+    /**
+     * 查找可以使用这个教室以及老师的课程信息
+     *
+     * @param roomId 教室ID
+     * @param teacherId 老师ID
+     * @param size  数量
+     * @return List<课程></>
+     */
+    List<CourseInfo> listCouldUsed(@Param("roomId") Long roomId, @Param("teacherId") Long teacherId,
+                                   @Param("size") Integer size, @Param("siteIdList") List<Long> siteIdList
+    );
 }
