610393f64625bc5c97fcf0a3aecd6a3216731552
yanyifei <yanyifei@zcckj.com>
2019-07-02 11:14:52 +0800

    初始化

--- education-common/src/main/java/com/jimmy/common/utils/StringUtils.java
+++ education-common/src/main/java/com/jimmy/common/utils/StringUtils.java
@@ -9,6 +9,7 @@ import java.util.UUID;
  * @date: 2019/6/6/006.
  */
 public class StringUtils extends org.apache.commons.lang3.StringUtils {
+
     public static String uuid() {
         return UUID.randomUUID().toString().replace("-", "").toUpperCase();
     }
