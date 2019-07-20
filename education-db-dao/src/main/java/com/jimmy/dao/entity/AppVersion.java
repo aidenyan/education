7e4f6c0ef88f37672e70b77b9810c6e01f3a3c94
yanyifei <yanyifei@zcckj.com>
2019-07-20 17:32:08 +0800

    初始化

---
+++ education-db-dao/src/main/java/com/jimmy/dao/entity/AppVersion.java
@@ -0,0 +1,31 @@
+package com.jimmy.dao.entity;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class AppVersion extends BaseEntity {
+
+    /**
+     * app的名字
+     */
+    private String appName;
+    /**
+     * 下载连接
+     */
+    private String downloadUrl;
+    /**
+     * 版本时间
+     */
+    private String versionCode;
+    /**
+     * 版本名字
+     */
+    private String  versionName;
+    /**
+     * 文案
+     */
+    private String releaseNote;
+
+}
