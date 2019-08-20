82120467fcba6138c7cfab990a2861153c586af4
yanyifei <yanyifei@zcckj.com>
2019-08-20 17:51:44 +0800

    签到

---
+++ education-web-api/src/main/java/com/jimmy/web/api/controller/admin/RoleController.java
@@ -0,0 +1,21 @@
+package com.jimmy.web.api.controller.admin;
+
+import com.jimmy.web.api.controller.BaseController;
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @ClassName: ClassmateController
+ * @Description:
+ * @author: aiden
+ * @date: 2019/7/9/009.
+ */
+
+@Api(value = "½ÇÉ«", description = "½ÇÉ«API")
+@Controller
+@RequestMapping("/admin/role")
+public class RoleController extends BaseController {
+
+
+}
